d5fb063477e3ecea6a02a838dc04373d9922281d
Crowdbotics <team@crowdbotics.com>
2021-04-29 17:55:40 +0000

    Update models

--- backend/connectors_demo_dev_22184/wsgi.py
+++ backend/connectors_demo_dev_22184/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connectors_demo_dev_22184.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "connectors_demo_dev_22184.settings")
 
 application = get_wsgi_application()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,17 @@
+from django.conf import settings
 from django.db import models
+
+
+class Product(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=100,
+    )
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
